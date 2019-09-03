620ee98cfd63487f2fb4d798431fc287dfa4cdaa
Ryan Westlund <rlwestlund@gmail.com>
2019-09-03 17:57:36 -0400

    Add the install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+alias cmd="rsync -cr --delete"
+cmd config.fish ~/.config/fish/
+cmd home.nanorc ~/.nanorc
+cmd nano ~/.config
+cmd dwmconf.h ~/.config/
+cmd home.conkyrc ~/.conkyrc
+cmd home.xinitrc ~/.xinitrc
+cmd qutebrowser_config.py ~/.config/qutebrowser/config.py
+cmd mplayer/ ~/.mplayer

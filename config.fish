c4b342113c62aaea904e6fe0117be82d3c22fecb
Ryan Westlund <rlwestlund@gmail.com>
2019-09-09 22:18:13 -0400

    Add ccount, a function to count commits in git

--- config.fish
+++ config.fish
@@ -64,6 +64,8 @@ function alarm; echo "~/code/utils/alarm.sh" | at $argv; end
 function al; $argv; if test $status -ne 0; beep; end; end
 # 'remind' - run a command and beep when it finishes regardless of exit status
 function re; $argv; beep; end
+# commit count
+function ccount; git log | grep "Author: " | wc -l; end
 
 function fish_prompt
 	# We have to save the exit status, otherwise the other commands run in the prompt will overwrite it.

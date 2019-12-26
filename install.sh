cd3ee428b3d7e10657a1e99da62126c2be06032b
Ryan Westlund <rlwestlund@gmail.com>
2019-12-26 13:56:06 -0500

    Add mkdir -p commands

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 alias cmd="rsync -cr --delete"
+mkdir -p ~/.config/fish
 cmd config.fish ~/.config/fish/
 cmd home.nanorc ~/.nanorc
 cmd nano ~/.config/
 cmd dwmconf.h ~/.config/
 cmd home.conkyrc ~/.conkyrc
 cmd home.xinitrc ~/.xinitrc
+mkdir -p ~/.config/qutebrowser
 cmd qutebrowser_config.py ~/.config/qutebrowser/config.py
 cmd mpv ~/.config/

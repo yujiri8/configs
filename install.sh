aed5735ecd7c276efbcbb94bc0bfe7e92863fcc9
Ryan Westlund <rlwestlund@gmail.com>
2020-08-07 12:23:48 -0400

    update mirco config
    * don't vendor the plugins, install them in install.sh
    * add keybinds for bounce plugin

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 alias cmd="rsync -crl --delete"
 mkdir -p ~/.config/fish
 cmd config.fish ~/.config/fish/
-cmd micro ~/.config/
 cmd dwmconf.h ~/.config/
 cmd home.conkyrc ~/.conkyrc
 cmd home.xinitrc ~/.xinitrc
 cmd home.xbindkeysrc ~/.xbindkeysrc
 cmd mpv ~/.config/
+cmd micro ~/.config/
+micro -plugin install go wc bounce quoter
